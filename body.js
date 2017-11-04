a5771d6a147f1b73c36a4774f5fabed6340e830c
LINDA\jess <jhu5749@gmail.com>
2017-11-03 19:02:07 -0700

    ughh

--- body.js
+++ body.js
@@ -15,88 +15,107 @@ firebase.initializeApp(config);
 var database = [];
 
 var db = firebase.database();
-var ref = db.ref("posts");
-ref.orderByChild("post_id").equalTo("1717731545171536_1939179983026690").once("value", function(snapshot) {
+var posts = db.ref("posts");
+posts.orderByChild("post_id").equalTo("1717731545171536_1939179983026690").once("value", function(snapshot) {
   console.log(snapshot.val());
 });
 
+var users = db.ref("users");
+var user_ids = db.ref("user_id");
+
 function main() {
-    $.getJSON( './post_database.json', function(data) {
-        data.forEach(function(d) {
-          if (d.post_id.length > 0) {
-                if (database[d.poster_name]) {
-                    database[d.poster_name].posts.push(d.post_id);
-                    database[d.poster_name].num_likes = database[d.poster_name].num_likes + d.num_likes;
-                    database[d.poster_name].num_comments = database[d.poster_name].num_comments + d.num_comments;
-                } else {
-                    database[d.poster_name] = {'poster_id' : d.poster_id,
-                                               'posts' : [d.post_id],
-                                               'num_likes' : d.num_likes,
-                                               'num_comments' : d.num_comments};
+    draw('Riley Woo');
+
+    function draw(userName) {
+        var poster_id = database[userName].poster_id;
+        if (poster_id) {
+            createReactBar(userName, poster_id);
+            console.log('drawing...');
+            FB.api(
+              "/" + poster_id + "/picture?type=large",
+              function (response) {
+                if (response && !response.error) {
+                    $("#picture").attr("src",response.data.url);
                 }
-            }
-        });
-
-        draw('Riley Woo');
-
-        function draw(userName) {
-            var poster_id = database[userName].poster_id;
-            if (poster_id) {
-                createReactBar(userName, poster_id);
-                console.log('drawing...');
-                FB.api(
-                  "/" + poster_id + "/picture?type=large",
-                  function (response) {
-                    if (response && !response.error) {
-                        $("#picture").attr("src",response.data.url);
-                    }
-                  }
-                );
-            } else {
-                console.log('what r u doin');
-            }
+              }
+            );
+        } else {
+            console.log('what r u doin');
         }
+    }
 
-        $("#user-form").submit(function(e) {
-            e.preventDefault();
-            console.log($('#user-name').val());
-            draw($('#user-name').val());
-            $('#user-name').val('');
-        });
+    $("#user-form").submit(function(e) {
+        e.preventDefault();
+        console.log($('#user-name').val());
+        draw($('#user-name').val());
+        $('#user-name').val('');
+    });
 
-        function insert(val) {
-          document.getElementById("inserthere").innerHTML = val;
+    function insert(val) {
+      document.getElementById("inserthere").innerHTML = val;
+    }
+        
+//        {
+//            "comments": {
+//                "data": [],
+//                "summary": {
+//                    "can_comment": false,
+//                    "order": "chronological",
+//                    "total_count": 25
+//                }
+//            },
+//            "created_time": "2017-08-18T19:43:22+0000",
+//            "id": "1717731545171536_1967971476814207",
+//            "likes": {
+//                "data": [],
+//                "summary": {
+//                    "can_like": true,
+//                    "has_liked": false,
+//                    "total_count": 150
+//                }
+//            },
+//            "link": "https://www.facebook.com/photo.php?fbid=1385535668160151&set=gm.1967971476814207&type=3"
+//        }
+        
+    FB.api(
+        "/1717731545171536/feed/?fields=link,created_time,name,id,likes.limit(0).summary(true),comments.limit(0).summary(true),shares&access_token=443809049300463|e6ff2a431bb3da7624faefbf39a15a3d",
+        function (response) {
+          if (response && !response.error) {
+            draw('Christopher Cox');
+            $('#user-name').val(response.data[0].likes.summary.total_count);
+          }
         }
+    );
 
-        function createReactBar(name, input) {
-          $(function () {
-            var myChart = Highcharts.chart('bar chart', {
-              chart: {
-                type: 'bar'
-              },
-              title: {
-                text: 'Reaction Data Breakdown'
-              },
-              xAxis: {
-                categories: ['Total Likes', 'Total Comments']
-              },
-              yAxis: {
-                title: {
-                  text: 'Reactions to posts'
-                },
-                type: 'linear',
-                tickInterval: 50,
-              },
-              series: [
+    function createReactBar(name, input) {
+      $(function () {
+        var myChart = Highcharts.chart('bar chart', {
+          chart: {
+            type: 'bar'
+          },
+          title: {
+            text: 'Reaction Data Breakdown'
+          },
+          xAxis: {
+            categories: ['Total Likes', 'Total Comments']
+          },
+          yAxis: {
+            title: {
+              text: 'Reactions to posts'
+            },
+            type: 'linear',
+            tickInterval: 50,
+          },
+            series: [
                 {
-                  data: formatUserData(name),
-                  name: name
+                    data: formatUserData(name),
+                    name: name
                 }
-              ],
-              credits: false
-            });
-          });
-        }
+            ],
+          credits: false
+        });
+      });
+    }
 
         function formatUserData(userName) {
           return Object.values([database[userName].num_likes, database[userName].num_comments]);
@@ -148,5 +167,4 @@ function main() {
 
         // Hannah Bockley
         // Seth Van Doren
-    });
 }
